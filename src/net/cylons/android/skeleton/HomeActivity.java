a4983271a5f4555b020291d510bf0123b680eba7
Willy Noel <willy@smartnsoft.com>
2013-11-20 20:51:01 +0100

    Integrate the navigation drawer.

--- src/net/cylons/android/skeleton/HomeActivity.java
+++ src/net/cylons/android/skeleton/HomeActivity.java
@@ -1,9 +1,13 @@
 package net.cylons.android.skeleton;
 
+import net.cylons.android.skeleton.app.SkeletonActivityAggregate;
 import android.content.Intent;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
-import net.cylons.android.skeleton.app.SkeletonActivityAggregate;
 
 import com.smartnsoft.droid4me.LifeCycle.BusinessObjectsRetrievalAsynchronousPolicyAnnotation;
 import com.smartnsoft.droid4me.app.SmartActivity;
@@ -19,13 +23,24 @@ public final class HomeActivity
     extends SmartActivity<SkeletonActivityAggregate>
 {
 
+  private DrawerLayout drawerLayout;
+
+  private ActionBarDrawerToggle drawerToggle;
+
+  @Override
   public void onRetrieveDisplayObjects()
   {
     setContentView(R.layout.home);
-    // TODO Auto-generated method stub
-
+    drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+    drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+    drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.drawable.ic_drawer, R.string.Home_menu_about, R.string.Home_menu_settings);
+    drawerToggle.setDrawerIndicatorEnabled(true);
+    drawerLayout.setDrawerListener(drawerToggle);
+    drawerToggle.syncState();
+    setActionBarBehavior();
   }
 
+  @Override
   public void onRetrieveBusinessObjects()
       throws BusinessObjectUnavailableException
   {
@@ -33,12 +48,14 @@ public final class HomeActivity
 
   }
 
+  @Override
   public void onFulfillDisplayObjects()
   {
     // TODO Auto-generated method stub
 
   }
 
+  @Override
   public void onSynchronizeDisplayObjects()
   {
     // TODO Auto-generated method stub
@@ -72,6 +89,94 @@ public final class HomeActivity
         });
 
     return true;
+  }
+
+  private void setActionBarBehavior()
+  {
+
+    if (getActionBar() != null)
+    {
+      drawerToggle.setDrawerIndicatorEnabled(true);
+      getActionBar().setDisplayHomeAsUpEnabled(true);
+      getActionBar().setDisplayShowHomeEnabled(true);
+      getActionBar().setHomeButtonEnabled(true);
+      getActionBar().setDisplayShowTitleEnabled(true);
+
+      // getActionBar().setDisplayShowTitleEnabled(false);
+      // getActionBar().setDisplayUseLogoEnabled(false);
+      // getActionBar().setDisplayHomeAsUpEnabled(true);
+      // getActionBar().setDisplayShowHomeEnabled(true);
+      // getActionBar().setHomeButtonEnabled(true);
+      // final LogoIconTitleBehavior logoIconTitleBehavior = parameters.logoIconTitleBehavior();
+      // if (logoIconTitleBehavior == LogoIconTitleBehavior.UseLogo)
+      // {
+      // getActionBar().setDisplayShowTitleEnabled(false);
+      // getActionBar().setDisplayUseLogoEnabled(true);
+      // getActionBar().setLogo(R.drawable.bar_accor_logo);
+      // }
+      // else if (logoIconTitleBehavior == LogoIconTitleBehavior.UseIcon)
+      // {
+      // getActionBar().setIcon(R.drawable.ic_bar_menu);
+      // }
+      // else if (logoIconTitleBehavior == LogoIconTitleBehavior.UseTitle)
+      // {
+      // getActionBar().setDisplayShowTitleEnabled(true);
+      // getActionBar().setIcon(android.R.color.transparent);
+      // }
+      //
+      // if (parameters.menuBehavior() == MenuBehavior.ShowUp)
+      // {
+      // drawerToggle.setDrawerIndicatorEnabled(false);
+      // getActionBar().setDisplayShowHomeEnabled(false);
+      // drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+      // }
+      // else if (parameters.menuBehavior() == MenuBehavior.ShowDrawer)
+      // {
+      // drawerToggle.setDrawerIndicatorEnabled(true);
+      // }
+      // else
+      // {
+      // getActionBar().setDisplayShowHomeEnabled(false);
+      // getActionBar().setDisplayHomeAsUpEnabled(false);
+      // drawerToggle.setDrawerIndicatorEnabled(false);
+      // drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+      // }
+    }
+  }
+
+  public void toggleDrawer()
+  {
+    if (drawerToggle.isDrawerIndicatorEnabled() == true)
+    {
+      if (drawerLayout.isDrawerOpen(Gravity.LEFT) == false)
+      {
+        drawerLayout.openDrawer(Gravity.LEFT);
+      }
+      else
+      {
+        drawerLayout.closeDrawers();
+      }
+    }
+    else
+    {
+      onBackPressed();
+    }
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item)
+  {
+    /**
+     * Set custom action on ActionBar home button to open SlidingMenu.
+     */
+    switch (item.getItemId())
+    {
+    case android.R.id.home:
+      toggleDrawer();
+      return true;
+    default:
+      return super.onOptionsItemSelected(item);
+    }
   }
 
 }
